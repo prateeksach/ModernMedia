93bfc38c603a9074e7f03e97bfb6717b6a0bd426
unknown <rajtejas@EECS-rajtejas.adsroot.itcs.umich.edu>
2015-03-23 18:44:34 -0400

    Added some skeleton code to read from and write to the Parse database.

--- scrape_newspapers.py
+++ scrape_newspapers.py
@@ -8,20 +8,68 @@ of URLs and continue from there while I make the API for you tonight.
 
 # Using newspaper library. Download and install from http://newspaper.readthedocs.org/en/latest/
 import newspaper 
+import os, sys
+import datetime
+
+# install ParsePy library: https://github.com/dgrtwo/ParsePy.git
+from parse_rest.connection import register, ParseBatcher
+from parse_rest.datatypes import Object as ParseObject
+
 
 # Function getURL which pulls some URL out of a database. Returns a url.
+# Parse to python tutorial is found here: https://github.com/dgrtwo/ParsePy
+"""
+def saveToParse(urlDictionary):
+
+        batcher = ParseBatcher()
+        batcher.batchsave(urlDictionary)
+
+def queryFromParse(AllURLs,read_query):
+        # Assumed that database contains table with fields: url, read_status
+        # url contains the urls, read_status indicates "read" or "unread"
+        urlquery = list(ParseObject.Query.filter(read_status == read_query))
+        return urlquery
+
+
+
+"""
 
 if __name__ == '__main__':
 
-    # Output of getURL() goes here. Using a temporary URL to scrape.
-    url = "http://www.cnn.com/2015/03/23/world/isis-luring-westerners/index.html"
+        """
+        APPLICATION_ID = "ENTER_APP_ID"
+        REST_API_KEY = "ENTER REST API KEY"
+        register(APPLICATION_ID, REST_API_KEY)
+        
+        # Instantiate parse.com object imported as ParseObject
+        class AllURLs(ParseObject):
+                pass
 
-    art = newspaper.Article(url)
-    art.download()
-    art.parse()
+        # Get a list of unread URLs
+        unreadURL = queryFromParse(AllURLs, "unread")
 
-    art_txt = art.text
+        for url in unreadURL.keys():
+        ### DO THE SCRAPING BELOW.
 
-    # art_txt -> ML algorithms for text processing. 
-    # Update Parse DB that URL has been processed.
-    
+        """
+
+	# Output of getURL() goes here. Using a temporary URL to scrape.
+	url = "http://www.cnn.com/2015/03/23/world/isis-luring-westerners/index.html"
+
+	art = newspaper.Article(url)
+	art.download()
+	art.parse()
+
+	art_txt = art.text
+
+        """
+        unreadURL[url] = "read"
+        ### DONE.
+        """
+	# art_txt -> ML algorithms for text processing. 
+
+        """
+        # Update the parse database indicating that URL has been read
+        savetoParse(unreadURL)
+
+        
