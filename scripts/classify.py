b838c4713351c14e717402e7ec758cb889ddf5e3
Yogesh Seetharaman <yogeshs@umich.edu>
2015-04-02 22:04:53 -0400

    loading from files

--- scripts/classify.py
+++ scripts/classify.py
@@ -8,9 +8,12 @@ training_folder = "data/"
 categories = {'yellow': ('yellow', 'nonyellow'),
               'political': ('conservative', 'liberal', 'neutral'),
               'position': ('critical', 'defensive', 'factual')}
+categoryTargets = {'yellow': {'yellow': 1, 'nonyellow': 2},
+                   'political': {'conservative': 1, 'liberal': 2, 'neutral': 3},
+                   'position': {'critical': 1, 'defensive': 2, 'factual': 3}}
 
 def organizeData():
-    trainingData = {}
+    trainingData = {} # { 'data': ['article1', 'article2'], 'yellow': [targets], ... }
 
     # data -> ['article1', 'article2', ...]
     # target -> [1, 2, 3, 2, 1, 3, ...] 1 = conservative, 2 = liberal, ... etc.
@@ -21,9 +24,13 @@ def organizeData():
 
     files = os.listdir(training_folder)
     for training_file in files:
+        # read in file, assign target for each category
         with open(training_folder + training_file, 'r') as f:
-            s = ''
-            # read in file, assign target for each category
+            json_file = json.load(f)
+            trainingData['data'].append(json_file['content'])
+            for category in categories:
+                label = json_file[category]
+                trainingData[category].append(categoryTargets[category][label])
 
 def trainClassifier(data):
     classifiers = {}
@@ -34,7 +41,7 @@ def trainClassifier(data):
                                                    penalty='l2',\
                                                    alpha=1e-3,
                                                    n_iter=5))])
-        _ = text_clf.fit(data[category]['data'], data[category]['target'])
+        _ = text_clf.fit(data['data'], data[category])
         classifiers[category] = text_clf
 
     return classifiers
